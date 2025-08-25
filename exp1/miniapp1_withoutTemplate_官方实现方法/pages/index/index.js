599e936757389350ae221e6ff68623c4bf700879
eric_zht <1935519509@qq.com>
2025-08-25 19:11:22 +0800

    exp1

---
+++ exp1/miniapp1_withoutTemplate_官方实现方法/pages/index/index.js
@@ -0,0 +1,109 @@
+// pages/index/index.js
+// index.js
+const defaultAvatarUrl = '/images/logo.png'
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+
+  // 点击头像（chooseAvatar）
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    const { nickName } = this.data.userInfo
+    this.setData({
+      "userInfo.avatarUrl": avatarUrl,
+      hasUserInfo: !!nickName && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+
+  // 输入昵称
+  onInputChange(e) {
+    const nickName = e.detail.value
+    const { avatarUrl } = this.data.userInfo
+    this.setData({
+      "userInfo.nickName": nickName,
+      hasUserInfo: !!nickName && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+
+  // 官方推荐接口：需要用户点击确认弹窗
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        console.log("wx.getUserProfile 返回:", res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      },
+      fail: (err) => {
+        console.log("用户拒绝授权:", err)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
